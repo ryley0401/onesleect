cdfa746347cdb56da92c8ee8e69ff890a06390c7
ryley0401 <worldpeace0401@gmail.com>
2025-07-04 10:31:15 +0800

    Add files via upload

--- oneselect/app.py
+++ oneselect/app.py
@@ -6,10 +6,36 @@ from flask import request
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
 
+@app.route('/')
+def root():
+    return redirect(url_for('oneselect'))
+
 #首頁路由
 @app.route("/oneselect")
 def oneselect():
     return render_template("index.html")
+
+#服飾頁面路由
+@app.route("/cloth")
+def cloth():
+    return render_template("cloth.html")
+
+#雜貨首頁
+@app.route("/Groceries")
+def Groceries():
+    return render_template("Groceries.html")
+
+#鞋類首頁
+@app.route("/shoes")
+def shoes():
+    return render_template("shoes.html")
+
+#所有商品頁面
+@app.route("/all")
+def all():
+    return render_template("all.html")
+
+
 
 
 if __name__ == '__main__':
