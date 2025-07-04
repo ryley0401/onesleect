3d69d464097bcb4ed3dd490888bd5cb97e8508b3
何松恆 <ryley0401@hesonghengs-MacBook-Air.local>
2025-07-04 15:56:41 +0800

    完成頁面註冊與資料庫連線

---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask
+from flask import flash
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import request
+import pymongo
+from pymongo.errors import ConnectionFailure, PyMongoError
+#加密
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+#初始化資料庫連線
+try:
+    client = pymongo.MongoClient("mongodb+srv://ryley0401:admin@oneselect.mrsaxx1.mongodb.net/")
+    client.admin.command('ping')
+    db = client.member_system
+    print("資料庫連線成功")
+except ConnectionFailure:
+    print('無法連線到資料庫')
+except PyMongoError as e:
+    print(f"其他pymongo錯誤：{ e }")
+
+
+
+
+
+app = Flask(__name__, static_folder="static", static_url_path="/static")
+app.secret_key = "oneselectkey"
+
+@app.route('/')
+def root():
+    return redirect(url_for('oneselect'))
+
+#首頁路由
+@app.route("/oneselect")
+def oneselect():
+    return render_template("index.html")
+
+#登入頁面設定
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+        email = request.form.get("email")
+        birthday = request.form.get("birthday", "")
+        gender = request.form.get("gender", "")
+        #基本欄位檢查
+        if not username or not password or not confirm_password or not email:
+            flash("請填寫所有必填欄位")
+            return render_template("signup.html")
+        if password != confirm_password:
+            flash("兩次輸入的密碼不一致")
+            return render_template("signup.html")
+        collection = db.users
+        if collection.find_one({"username" : username}):
+            flash("此帳號已被註冊，請重新輸入")
+            return render_template("signup.html")
+        if collection.find_one({"email" : email}):
+            flash("信箱已被使用，請更換一個信箱")
+            return render_template("signup.html")
+        #密碼加密
+        hashed_password = generate_password_hash(password)
+
+        #寫入資料庫
+        user_data = {
+            'username' : username,
+            'password' : hashed_password,
+            'email' : email,
+            'birthday' : birthday,
+            'gender' : gender
+        }
+        collection.insert_one(user_data)
+        flash("註冊成功，請登入")
+        return redirect(url_for("login"))
+    return render_template("signup.html")
+
+
+
+#服飾頁面路由
+@app.route("/cloth")
+def cloth():
+    return render_template("cloth.html")
+
+#雜貨首頁
+@app.route("/Groceries")
+def Groceries():
+    return render_template("Groceries.html")
+
+#鞋類首頁
+@app.route("/shoes")
+def shoes():
+    return render_template("shoes.html")
+
+#所有商品頁面
+@app.route("/all")
+def all():
+    return render_template("all.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
